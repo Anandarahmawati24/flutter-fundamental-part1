530a05c2532681229c8f966a363f82d890576bed
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-09-27 23:21:38 +0700

    praktikum 5 langkah 3 scaffold

---
+++ lib/basic_widgets/scaffold_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Step3Scaffold extends StatefulWidget {
+  const Step3Scaffold({Key? key}) : super(key: key);
+
+  @override
+  State<Step3Scaffold> createState() => _Step3ScaffoldState();
+}
+
+class _Step3ScaffoldState extends State<Step3Scaffold> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Increment App")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
