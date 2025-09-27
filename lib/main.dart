6c26f3d658843b0e310d08a97dfbdf19034037ed
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-09-27 23:25:21 +0700

    praktikum 5 langkah 4

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hello_world/basic_widgets/image_widget.dart';
 import 'package:hello_world/basic_widgets/loading_cupertino.dart';
 import 'package:hello_world/basic_widgets/text_widget.dart';
 import 'package:hello_world/basic_widgets/scaffold_widget.dart';
+import 'package:hello_world/basic_widgets/dialog_widget.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Praktikum 5',
-      home: Step3Scaffold(), // Ganti sesuai step yang ingin dijalankan
+      home: Step4Dialog(), // Ganti sesuai step yang ingin dijalankan
     );
   }
 }
